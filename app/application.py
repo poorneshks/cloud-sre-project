c7a4700da144ae7ca1ed7e1e6e5cb83e64557e08
poornesh <ks.poornesh@gmail.com>
2025-07-13 20:43:09 +0530

    fix: renamed app.py to application.py for Azure compatibility

---
+++ app/application.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "👋 Hello from Flask SRE App!"
+
+if __name__ == "__main__":
+    # Use Flask's built-in development server when running locally
+    app.run(debug=True, host="0.0.0.0", port=5000)
